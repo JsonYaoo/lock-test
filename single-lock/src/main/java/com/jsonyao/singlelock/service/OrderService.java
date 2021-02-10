73cee5bf30ebbaebe5608028ee79b4ca61d3f32f
yaocs2 <yaocs2@midea.com>
2021-02-10 23:37:03 +0800

    lock-test> single-lock> 测试超卖情况2

--- single-lock/src/main/java/com/jsonyao/singlelock/service/OrderService.java
+++ single-lock/src/main/java/com/jsonyao/singlelock/service/OrderService.java
@@ -42,7 +42,7 @@ public class OrderService {
      * @throws Exception
      */
     @Transactional(rollbackFor = Exception.class)
-    public Integer createOrder() throws Exception{
+    public Integer createOrder01() throws Exception{
         Product product = null;
 
         product = productMapper.selectByPrimaryKey(purchaseProductId);
@@ -58,12 +58,62 @@ public class OrderService {
             throw new Exception("商品"+purchaseProductId+"仅剩"+currentCount+"件，无法购买");
         }
 
-        // 1. 初始情况: 不加锁, 直接使用spring事务, 方法内更新库存
+        // 1. 超卖情况1: 不加锁, 直接使用spring事务, 方法内更新库存
         Integer leftCount = currentCount - purchaseProductNum;
         product.setCount(leftCount);
         product.setUpdateTime(new Date());
         product.setUpdateUser("xxx");
         productMapper.updateByPrimaryKeySelective(product);
+
+        Order order = new Order();
+        order.setOrderAmount(product.getPrice().multiply(new BigDecimal(purchaseProductNum)));
+        order.setOrderStatus(1);//待处理
+        order.setReceiverName("xxx");
+        order.setReceiverMobile("13311112222");
+        order.setCreateTime(new Date());
+        order.setCreateUser("xxx");
+        order.setUpdateTime(new Date());
+        order.setUpdateUser("xxx");
+        orderMapper.insertSelective(order);
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrderId(order.getId());
+        orderItem.setProductId(product.getId());
+        orderItem.setPurchasePrice(product.getPrice());
+        orderItem.setPurchaseNum(purchaseProductNum);
+        orderItem.setCreateUser("xxx");
+        orderItem.setCreateTime(new Date());
+        orderItem.setUpdateTime(new Date());
+        orderItem.setUpdateUser("xxx");
+        orderItemMapper.insertSelective(orderItem);
+        return order.getId();
+    }
+
+    /**
+     * 2. 超卖情况2: 直接使用Spring事务, 利用数据库行锁, sql里增量更新库存
+     * => 出现超卖: 库存为-4, 有5个订单
+     * @return
+     * @throws Exception
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public Integer createOrder02() throws Exception{
+        Product product = null;
+
+        product = productMapper.selectByPrimaryKey(purchaseProductId);
+        if (product==null){
+            throw new Exception("购买商品："+purchaseProductId+"不存在");
+        }
+
+        //商品当前库存
+        Integer currentCount = product.getCount();
+        System.out.println(Thread.currentThread().getName()+"库存数："+currentCount);
+        //校验库存
+        if (purchaseProductNum > currentCount){
+            throw new Exception("商品"+purchaseProductId+"仅剩"+currentCount+"件，无法购买");
+        }
+
+        // 超卖情况2: 直接使用Spring事务, 利用数据库行锁, sql里增量更新库存
+        productMapper.updateProductCount(purchaseProductNum,"xxx",new Date(),product.getId());
 
         Order order = new Order();
         order.setOrderAmount(product.getPrice().multiply(new BigDecimal(purchaseProductNum)));
