a659299146053ec47097fe6147edb51a5b495df1
yaocs2 <yaocs2@midea.com>
2021-02-11 13:07:50 +0800

    lock-test> single-lock> 测试解决超卖情况4

--- single-lock/src/main/java/com/jsonyao/singlelock/service/OrderService.java
+++ single-lock/src/main/java/com/jsonyao/singlelock/service/OrderService.java
@@ -17,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @Slf4j
@@ -37,8 +39,8 @@ public class OrderService {
     private int purchaseProductId = 100100;
     //购买商品数量
     private int purchaseProductNum = 1;
-
-//    private Lock lock = new ReentrantLock();
+    // 可重入锁
+    private Lock lock = new ReentrantLock();
 
     /**
      * 1. 超卖情况1: 不加锁, 直接使用spring事务, 方法内更新库存
@@ -252,60 +254,52 @@ public class OrderService {
         return order.getId();
     }
 
-////    @Transactional(rollbackFor = Exception.class)
-//    public Integer createOrder() throws Exception{
-//        Product product = null;
-//
-//        lock.lock();
-//        try {
-//            TransactionStatus transaction1 = platformTransactionManager.getTransaction(transactionDefinition);
-//            product = productMapper.selectByPrimaryKey(purchaseProductId);
-//            if (product==null){
-//                platformTransactionManager.rollback(transaction1);
-//                throw new Exception("购买商品："+purchaseProductId+"不存在");
-//            }
-//
-//            //商品当前库存
-//            Integer currentCount = product.getCount();
-//            System.out.println(Thread.currentThread().getName()+"库存数："+currentCount);
-//            //校验库存
-//            if (purchaseProductNum > currentCount){
-//                platformTransactionManager.rollback(transaction1);
-//                throw
-//                        new Exception("商品"+purchaseProductId+"仅剩"+currentCount+"件，无法购买");
-//            }
-//
-//            productMapper.updateProductCount(purchaseProductNum,"xxx",new Date(),product.getId());
-//            platformTransactionManager.commit(transaction1);
-//        }finally {
-//            lock.unlock();
-//        }
-//
-//        TransactionStatus transaction = platformTransactionManager.getTransaction(transactionDefinition);
-//        Order order = new Order();
-//        order.setOrderAmount(product.getPrice().multiply(new BigDecimal(purchaseProductNum)));
-//        order.setOrderStatus(1);//待处理
-//        order.setReceiverName("xxx");
-//        order.setReceiverMobile("13311112222");
-//        order.setCreateTime(new Date());
-//        order.setCreateUser("xxx");
-//        order.setUpdateTime(new Date());
-//        order.setUpdateUser("xxx");
-//        orderMapper.insertSelective(order);
-//
-//        OrderItem orderItem = new OrderItem();
-//        orderItem.setOrderId(order.getId());
-//        orderItem.setProductId(product.getId());
-//        orderItem.setPurchasePrice(product.getPrice());
-//        orderItem.setPurchaseNum(purchaseProductNum);
-//        orderItem.setCreateUser("xxx");
-//        orderItem.setCreateTime(new Date());
-//        orderItem.setUpdateTime(new Date());
-//        orderItem.setUpdateUser("xxx");
-//        orderItemMapper.insertSelective(orderItem);
-//        platformTransactionManager.commit(transaction);
-//        return order.getId();
-//    }
+    /**
+     * 7. 解决超卖情况4: 使用可重入锁对扣减库存代码进行加锁, 同时手动对事务加锁
+     * => 解决了超卖: 库存正常, 订单正常, 但需要避免嵌套事务, 以及在finally处释放锁, 防止发生死锁
+     * @return
+     * @throws Exception
+     */
+    public Integer createOrder07() throws Exception{
+        Product product = null;
+
+        lock.lock();
+        try {
+            // 手工开启事务
+            TransactionStatus transactionStatus = platformTransactionManager.getTransaction(transactionDefinition);
+            product = productMapper.selectByPrimaryKey(purchaseProductId);
+            if (product==null){
+                // 手工回滚事务
+                platformTransactionManager.rollback(transactionStatus);
+                throw new Exception("购买商品："+purchaseProductId+"不存在");
+            }
+
+            //商品当前库存
+            Integer currentCount = product.getCount();
+            System.out.println(Thread.currentThread().getName()+"库存数："+currentCount);
+
+            //校验库存
+            if (purchaseProductNum > currentCount){
+                // 手工回滚事务
+                platformTransactionManager.rollback(transactionStatus);
+                throw new Exception("商品"+purchaseProductId+"仅剩"+currentCount+"件，无法购买");
+            }
+
+            productMapper.updateProductCount(purchaseProductNum,"xxx",new Date(),product.getId());
+            // 手工提交事务
+            platformTransactionManager.commit(transactionStatus);
+        } finally {
+            lock.unlock();
+        }
+
+        // 手工开启事务: 避免事务嵌套
+        TransactionStatus transactionStatus = platformTransactionManager.getTransaction(transactionDefinition);
+        Order order = getOrder(product);
+        // 手工提交事务: 避免事务嵌套
+        platformTransactionManager.commit(transactionStatus);
+
+        return order.getId();
+    }
 
     /**
      * 根据Product创建订单
