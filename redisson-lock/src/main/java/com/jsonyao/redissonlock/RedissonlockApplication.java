e9d5c7e8a195d2683d823702d446a814befa7e0c
yaocs2 <yaocs2@midea.com>
2021-02-12 13:22:56 +0800

    lock-test> distribute-zk-lock> Redisson分布式锁与Spring XML测试

--- redisson-lock/src/main/java/com/jsonyao/redissonlock/RedissonlockApplication.java
+++ redisson-lock/src/main/java/com/jsonyao/redissonlock/RedissonlockApplication.java
@@ -2,11 +2,13 @@ package com.jsonyao.redissonlock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Redisson Lock测试应用
  */
 @SpringBootApplication
+@ImportResource(locations = "classpath:redisson.xml")
 public class RedissonlockApplication {
 
     public static void main(String[] args) {
--- redisson-lock/src/main/java/com/jsonyao/redissonlock/controller/RedissonLockController.java
+++ redisson-lock/src/main/java/com/jsonyao/redissonlock/controller/RedissonLockController.java
@@ -6,6 +6,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,9 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @Slf4j
 public class RedissonLockController {
+
+    @Autowired
+    private RedissonClient springRedissonClient;
 
     @RequestMapping("/redissonLock")
     public String redissonLock(){
@@ -31,6 +35,32 @@ public class RedissonLockController {
 
         // 获取Redis分布式锁
         RLock lock = redissonClient.getLock("/order");
+        try {
+            // 获取不到会阻塞, 30代表的是过期时间, 锁30s不放开则自动释放
+            lock.lock(30, TimeUnit.SECONDS);
+            log.info("我获得了锁！！！");
+            Thread.sleep(10000);// 40000处理时间如果大于过期时间, 在后面释放锁的时候会抛出java.lang.IllegalMonitorStateException
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+            log.info("我释放了锁！！");
+        }
+
+        log.info("方法执行完成！！");
+        return "方法执行完成！！";
+    }
+
+    /**
+     * 测试Redisson分布式锁与Spring XML/SpringBoot
+     * @return
+     */
+    @RequestMapping("/springRedissonLock")
+    public String springRedissonLock(){
+        log.info("我进入了方法！！");
+
+        // 获取Redis分布式锁
+        RLock lock = springRedissonClient.getLock("/order");
         try {
             // 获取不到会阻塞, 30代表的是过期时间, 锁30s不放开则自动释放
             lock.lock(30, TimeUnit.SECONDS);
